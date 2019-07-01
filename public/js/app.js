dc2ad1d80fe86d1630fdb30ed328754570fb33d2
Askie Lin <isme408@gmail.com>
2019-07-01 14:54:15 +0800

    compile askie's app.js

--- public/js/app.js
+++ public/js/app.js
@@ -50418,8 +50418,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/ttnppedr/code/gipay/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/ttnppedr/code/gipay/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/askie/project/side-project/gipay/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/askie/project/side-project/gipay/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
