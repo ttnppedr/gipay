4bc9c869de70be5501189d31203c997701f344d7
Askie Lin <isme408@gmail.com>
2019-07-03 15:28:32 +0800

    [lint] Lint code

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -27,7 +27,9 @@
       <div class="navbar-end">
         <div class="navbar-item">
           <div class="admin-info">
-            <div class="admin-info__avatar"><img :src="adminInfo.avatar" /></div>
+            <div class="admin-info__avatar">
+              <img :src="adminInfo.avatar" />
+            </div>
             <div class="admin-info__name">{{ adminInfo.name }}</div>
           </div>
           <div class="buttons" @click="logout">
@@ -40,77 +42,73 @@
 </template>
 
 <script>
-import logo from "../../images/logo.svg"
-export default {
-  data: function () {
-    return {
-      token: window.$cookies.get("token"),
-      logo: logo,
-      adminInfo: '',
-    };
-  },
-  created () {
-    this.getAdminInfo();
-  },
-  computed: {
-    loginStatusMsg () {
-      return this.token ? "登出" : "登入";
-    }
-  },
-  methods: {
-    async getAdminInfo () {
-      // 初始化，登入中才拿管理者資訊
-      if (!this.token && window.location.pathname !== '/admin-login') {
-        window.location.replace('/admin-login');
+  import logo from '../../images/logo.svg';
+  export default {
+    data: function() {
+      return {
+        token: window.$cookies.get('token'),
+        logo: logo,
+        adminInfo: ''
+      };
+    },
+    created() {
+      this.getAdminInfo();
+    },
+    computed: {
+      loginStatusMsg() {
+        return this.token ? '登出' : '登入';
       }
-      try {
-        const adminInfo = await axios
-          .get('https://gipay.xyz/api/info', {
+    },
+    methods: {
+      async getAdminInfo() {
+        // 初始化，登入中才拿管理者資訊
+        if (!this.token && window.location.pathname !== '/admin-login') {
+          window.location.replace('/admin-login');
+        }
+        try {
+          const adminInfo = await axios.get('https://gipay.xyz/api/info', {
             headers: {
               Authorization: `Bearer ${this.token}`,
               'Content-Type': 'application/json',
               Accept: 'application/json'
             }
-          })
+          });
 
-        if(adminInfo.data.message === "token error") {
-          this.logout();
-          return;
+          if (adminInfo.data.message === 'token error') {
+            this.logout();
+            return;
+          }
+
+          this.adminInfo = adminInfo.data;
+        } catch (e) {
+          console.log('error', e);
         }
-
-        this.adminInfo = adminInfo.data;
-
-      } catch (e) {
-        console.log('error', e);
+      },
+      logout() {
+        if (!this.token) return;
+        window.$cookies.remove('token');
+        window.location.replace('/admin-login');
       }
-    },
-    logout () {
-      if (!this.token) return;
-      window.$cookies.remove("token");
-      window.location.replace("/admin-login");
     }
-  }
-};
+  };
 </script>
 
-
-
 <style lang="scss" scoped>
-.admin-info {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  .admin-info {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  &__avatar {
-    font-size: 0;
-    img {
-      border-radius: 50%;
+    &__avatar {
+      font-size: 0;
+      img {
+        border-radius: 50%;
+      }
+    }
+
+    &__name {
+      margin-left: 1em;
+      margin-right: 1em;
     }
   }
-
-  &__name {
-    margin-left: 1em;
-    margin-right: 1em;
-  }
-}
 </style>
